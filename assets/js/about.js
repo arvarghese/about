460589bd0761d2e45805281b7448dcf9d3161cd4
Arun Varghese <avarghese@spireon.com>
2017-02-21 10:40:08 -0600

    added instagram feed, parallax

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,5 +3,33 @@ var About = {
         About.loadEvents();
     },
     loadEvents: function () {
-    }
+        About.loadInstafeed();
+        About.loadParallax();
+    },
+    loadParallax: function () {
+        $(window).on('scroll', function () {
+            window.requestAnimationFrame(function () {
+                var scrolled = $(window).scrollTop();
+                $('.splash-img').css({
+                    'transform': 'translate3d(0,' + scrolled * -0.3 + 'px, 0)'
+                });
+            });
+        });
+    },
+    loadInstafeed: function () {
+        About.instaFeed = new Instafeed({
+            target: "ig-feed",
+            get: 'user',
+            userId: 232162132,
+            accessToken: '232162132.62a228a.96de27b918d8401a858301d776a4aecb',
+            limit: 6,
+            sortBy: 'most-recent',
+            after: function () { },
+            links: true,
+            resolution: "low_resolution",
+            template: "\n\t\t\t\t\t<a href=\"{{link}}\" target=\"_blank\">\n\t\t\t\t\t\t<div class=\"ig-img\" style=\"background-image:url('http:{{image}}')\"/>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</a>\t\t\t\t\t\t\n\t\t\t"
+        });
+        About.instaFeed.run();
+    },
+    instaFeed: {}
 };
--- assets/js/about.ts
+++ assets/js/about.ts
@@ -2,7 +2,39 @@ let About = {
 	initialize: () => {
 		About.loadEvents();
 	},
-	loadEvents: () => {
-
-	}
-};
+	loadEvents: () => {		
+		About.loadInstafeed();
+		About.loadParallax();
+	},
+	loadParallax: () => {
+		$(window).on('scroll', () => {
+			window.requestAnimationFrame(() => {
+				let scrolled = $(window).scrollTop();
+				$('.splash-img').css({
+					'transform': 'translate3d(0,' + scrolled * -0.3 + 'px, 0)'
+				});
+			});
+		});
+	},
+	loadInstafeed: () => {
+		About.instaFeed = new Instafeed({
+			target: "ig-feed",
+			get: 'user',
+			userId: 232162132,
+			accessToken: '232162132.62a228a.96de27b918d8401a858301d776a4aecb',
+			limit: 6,
+			sortBy: 'most-recent',
+			after: () => {},
+			links: true,
+			resolution: "low_resolution",
+			template: `
+					<a href="{{link}}" target="_blank">
+						<div class="ig-img" style="background-image:url('http:{{image}}')"/>
+						</div>
+					</a>						
+			`
+		});
+		About.instaFeed.run();
+	},
+	instaFeed: {} 
+}
